a0dd3c947d1a91f8cdf0099507b53497a468b768
Robert Murphy <robert.a.murphy@wustl.edu>
2022-03-01 16:02:31 -0800

    Changed the sort again

--- Services/st.py
+++ Services/st.py
@@ -166,7 +166,7 @@ if type(fls) in [type([]),type(np.asarray([]))] and len(fls) > 0:
                         break
                     # define the inputs to the model
                     x    = pd.DataFrame(dat[:,cols].astype(np.single),columns=np.asarray(nhdr)[cols])
-                    x    = x.sort_values(by=list(range(0,len(x.columns))),axis=1)#sort_values(by=np.asarray(nhdr)[cols],axis=1)#.to_numpy()
+                    x    = x.sort_values(by=list(x.columns))#sort_values(by=list(range(0,len(x.columns))),axis=1)#sort_values(by=np.asarray(nhdr)[cols],axis=1)#.to_numpy()
                     # build a simple model
                     model= Sequential()
                     # relu at the input layer
